72f67ac843a4295e929573c16987231221b27e97
yakushinvl <development.yakushinvl@gmail.com>
2026-06-04 17:56:18 +0300

    Практическое занятие №22: балансировщик нагрузки Nginx

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Response from backend server",
+    port: PORT
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
